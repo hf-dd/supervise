c2155e8b389b9653ea5ea051f97d10efae87a64e
hf <admin@bjsxt.com>
2019-09-08 10:06:45 +0800

    测试使用第二次

--- src/main/java/com/hbu/service/impl/UserServiceImpl.java
+++ src/main/java/com/hbu/service/impl/UserServiceImpl.java
@@ -1,20 +1,11 @@
 package com.hbu.service.impl;
 
 import com.hbu.mapper.UserMapper;
-import com.hbu.pojo.User;
-import com.hbu.pojo.UserExample;
 import com.hbu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
-/**
- * @Auther:http://www.hf.com
- * @Date:2019/9/7
- * @Description:com.hbu.service.impl
- * @version:1.0
- */
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -22,7 +13,6 @@ public class UserServiceImpl implements UserService {
     private UserMapper userMapper;
 
     public void selAll() {
-        UserExample example = new UserExample();
-        List<User> list = userMapper.selectByExample(example);
+        System.out.println("hello");
     }
 }
