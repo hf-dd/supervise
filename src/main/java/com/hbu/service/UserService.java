5a8585e39f3275d3d4db485b17ba6fcb1c1df61f
hf <admin@bjsxt.com>
2019-09-08 10:28:21 +0800

    环境搭建完整版

--- src/main/java/com/hbu/service/UserService.java
+++ src/main/java/com/hbu/service/UserService.java
@@ -1,13 +1,5 @@
 package com.hbu.service;
 
-import com.hbu.pojo.User;
-
-/**
- * @Auther:http://www.hf.com
- * @Date:2019/9/7
- * @Description:com.hbu.service.impl
- * @version:1.0
- */
 public interface UserService {
     void selAll();
 }
--- src/main/java/com/hbu/service/impl/UserServiceImpl.java
+++ src/main/java/com/hbu/service/impl/UserServiceImpl.java
@@ -1,15 +1,20 @@
 package com.hbu.service.impl;
 
 import com.hbu.mapper.UserMapper;
+import com.hbu.pojo.User;
+import com.hbu.pojo.UserExample;
 import com.hbu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     public void selAll() {
